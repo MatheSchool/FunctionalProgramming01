aa4381e246f15ff53988a9deacf666de0071bcc1
Andrés Giménez <andres.gimenez@outlook.com>
2020-07-18 17:13:50 +0000

    Initial commit

---
+++ main.fs
@@ -0,0 +1,8 @@
+let squareIt = fun n -> n * n
+
+let squareIt2 n = n * n
+
+System.Console.WriteLine(squareIt 4)
+System.Console.WriteLine(squareIt 5)
+System.Console.WriteLine(squareIt 6)
+System.Console.WriteLine(squareIt 7)
